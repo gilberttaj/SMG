10db0b69044296836b23b4618976d16a1af79c92
gilberttaj <gilbert@bpoc.co.jp>
2023-02-15 09:46:16 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            this is footer part.
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+this is body part.
+
+<?php get_footer(); ?>
