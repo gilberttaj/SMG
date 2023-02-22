3ce6689186881b8963367f2323d6f896461b70da
Ahrve <ahrvekim@bpoc.co.jp>
2023-02-22 17:03:54 +0800

    personal and u15

--- header.php
+++ header.php
@@ -89,16 +89,16 @@
 <header id='header' class="fixed w-full top-[32px] bg-white z-[1]">
         <nav class="w-full flex flex-row smin-750:shadow-md">
             <div class="flex flex-row max-lg:h-20 max-smin-750:hidden w-[48%] max-xl:w-[50%]">
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> 
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer "> 
                     <h3 class="nav-title">ニュース</h3>
                 </div>
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> 
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> 
                     <h3 class="nav-title">日体大SMG横浜とは</h3>
                 </div>
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> 
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> 
                     <h3 class="nav-title">選手・チーム紹介</h3>
                 </div>
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> 
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> 
                     <h3 class="nav-title">スケジュール一覧</h3>
                 </div>
             </div>
@@ -108,10 +108,10 @@
             </div>
             <div class="max-smin-750:w-[40%] max-smin-750:block hidden"></div>
             <div class="flex flex-row max-lg:h-20 max-smin-750:hidden w-[43%]">
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> <h3 class="nav-title">競技・試合結果</h3></div>
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> <h3 class="nav-title">地域活動・<p>プロジェクト</p></h3></div>
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> <h3 class="nav-title">サポーターズクラブ<p>BLUES</p></h3></div>
-                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center"> <h3 class="nav-title"><p>BLUES</p>オンラインショップ</h3></div>
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> <h3 class="nav-title">競技・試合結果</h3></div>
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> <h3 class="nav-title">地域活動・<p>プロジェクト</p></h3></div>
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> <h3 class="nav-title">サポーターズクラブ<p>BLUES</p></h3></div>
+                <div class="nav-link text-[#004EA1] w-3/12 xl:py-4 smin-750:p-2 flex justify-center text-center items-center hover:bg-[#004EA1] hover:text-white cursor-pointer"> <h3 class="nav-title"><p>BLUES</p>オンラインショップ</h3></div>
             </div>
             <div class="nav-envelope flex justify-center items-center flex-col xl:w-[5%] w-[6%] max-lg:w-[8%] max-smin-750:hidden">
                 <i class="fa-solid fa-envelope text-4xl max-lg:text-3xl text-white max:md-lg"></i>
